043081fe7d95fa80f59cfdd036245c56a16e5834
keshav jha <keshavjhasre@gmail.com>
2026-03-09 10:53:57 +0530

    Initialize Three.js scene with rotating torus knot

---
+++ script.js
@@ -0,0 +1,40 @@
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+75,
+window.innerWidth/window.innerHeight,
+0.1,
+1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+canvas: document.querySelector('#bg')
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+camera.position.setZ(30);
+
+const geometry = new THREE.TorusKnotGeometry(10,3,100,16);
+
+const material = new THREE.MeshBasicMaterial({
+color:0x00ffff,
+wireframe:true
+});
+
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+function animate(){
+
+requestAnimationFrame(animate);
+
+torus.rotation.x += 0.01;
+torus.rotation.y += 0.005;
+
+renderer.render(scene, camera);
+
+}
+
+animate();
